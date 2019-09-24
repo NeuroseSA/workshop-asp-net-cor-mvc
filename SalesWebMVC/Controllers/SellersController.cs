a9fb91e9074eb69e62db1ec7467a4c2e75d5c0e7
Alex <alexjuniorsantos@live.com>
2019-09-24 13:42:36 -0400

    Delete Seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -41,5 +41,28 @@ namespace SalesWebMVC.Controllers
             _SellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _SellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _SellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
